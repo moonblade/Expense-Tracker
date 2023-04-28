77daa1f5824adcca5a93c0499b77a4afc0834d72
moonblade <moonblade168@gmail.com>
2023-04-28 07:28:09 +0530

    Add transaction state

--- backend/tracker.py
+++ backend/tracker.py
@@ -16,26 +16,28 @@ class Tracker():
             emails = self.mail.getEmailsFrom(sender)
             for email in emails:
                 for senderTemplate in template["senderTemplates"]:
-                    match = re.match(senderTemplate["subjectRegex"], email.subject)
-                    if match:
-                        matchGroups = match.groups()
-                        content = {}
-                        if len(matchGroups) == len(senderTemplate["groups"]):
-                            content = dict([(senderTemplate["groups"][index], matchGroups[index].strip().lower()) for index in range(len(matchGroups))])
-                            content["date"] = email.date
-                            soup = BeautifulSoup(email.htmlContent, features="lxml")
-                        if "contentTemplates" in senderTemplate:
-                            for contentTemplate in senderTemplate["contentTemplates"]:
-                                if contentTemplate["type"] == "findByText":
-                                    element = soup.find(lambda tag:tag.name == contentTemplate["tagName"] and tag.text.strip() == contentTemplate["text"])
-                                    if "findNext" in contentTemplate:
-                                        for nextTag in contentTemplate["findNext"]:
-                                            element = element.findNext(nextTag)
-                                    content[contentTemplate["key"]] = element.text.strip()
-
+                    try:
+                        match = re.match(senderTemplate["subjectRegex"], email.subject)
+                        if match:
+                            matchGroups = match.groups()
+                            content = {}
+                            if len(matchGroups) == len(senderTemplate["groups"]):
+                                content = dict([(senderTemplate["groups"][index], matchGroups[index].strip().lower()) for index in range(len(matchGroups))])
+                                content["date"] = email.date
+                                soup = BeautifulSoup(email.htmlContent, features="lxml")
+                            if "contentTemplates" in senderTemplate:
+                                for contentTemplate in senderTemplate["contentTemplates"]:
+                                    if contentTemplate["type"] == "findByText":
+                                        element = soup.find(lambda tag:tag.name == contentTemplate["tagName"] and tag.text.strip().lower() == contentTemplate["text"].lower())
+                                        if "findNext" in contentTemplate:
+                                            for nextTag in contentTemplate["findNext"]:
+                                                print(element)
+                                                element = element.findNext(nextTag)
+                                        content[contentTemplate["key"]] = element.text.strip()
                         print(content)
-                        if "payee" in content:
-                            print("="*20)
+                        print("="*20)
+                    except Exception as e:
+                        print(e)
 
 
 
