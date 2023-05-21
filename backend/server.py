b546983dfca6f6a3abdf3d4b3c6b18e12a21253c
mnishamk <mnishamk@commvault.com>
2023-05-21 11:00:49 +0530

    add cors

--- backend/server.py
+++ backend/server.py
@@ -20,6 +20,23 @@ def is_authenticated(user, password):
             return False
     return True
 
+class EnableCors(object):
+    name = 'enable_cors'
+    api = 2
+
+    def apply(self, fn, _):
+        def _enable_cors(*args, **kwargs):
+            # set CORS headers
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+            if request.method != 'OPTIONS':
+                # actual request; reply with the actual response
+                return fn(*args, **kwargs)
+
+        return _enable_cors
+
 @get('/expense')
 @auth_basic(is_authenticated)
 def getExpenses():
