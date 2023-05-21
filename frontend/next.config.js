ea4ca2cae6560a6d3cec0e92a0dc7b873a2f4fd5
moonblade <moonblade168@gmail.com>
2023-05-21 14:35:28 +0530

    update with next js static pages

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,9 @@
+const repo = 'Expense-Tracker'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   reactStrictMode: true
-};
+}
