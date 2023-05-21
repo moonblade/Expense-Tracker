449fe41dfe0db7eb7a45525e6b6fb11759b84622
moonblade <moonblade168@gmail.com>
2023-05-21 14:59:12 +0530

    not needed with custom domain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,10 +5,10 @@ let basePath = ''
 
 if (isGithubActions) {
   // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  // const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  // assetPrefix = `/${repo}/`
+  // basePath = `/${repo}`
 }
 
 module.exports = {
