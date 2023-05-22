9e7e6139d9640d871c927472c281241d8d84b398
mnishamk <mnishamk@commvault.com>
2023-05-22 22:06:18 +0530

    add list

--- frontend/src/sections/overview/spends.js
+++ frontend/src/sections/overview/spends.js
@@ -1,12 +1,32 @@
-import { Card, CardContent, MenuItem, Select } from "@mui/material";
-import { useContext, useState } from "react";
+import { Avatar, Card, CardContent, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { useContext } from "react";
 import { ExpenseContext } from "src/contexts/expenses";
+import categories from "./categories";
 import moment from "moment";
 
 export const Spends = () => {
+  const { expenses } = useContext(ExpenseContext);
+  console.log(expenses)
+
   return (
     <Card>
       <CardContent>
+        <List>
+          {
+            expenses.map(expense => (
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    {
+                      categories[expense.category]?.icon
+                    }
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={expense.payee} secondary={moment(expense.date.$date).format('MMMM Do YYYY, h:mm:ss a')} />
+              </ListItem>
+            ))
+          }
+        </List>
       </CardContent>
     </Card>
   )
