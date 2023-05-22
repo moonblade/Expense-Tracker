fdf209ae15002a3d9f0143c75c0932ba8651bd74
mnishamk <mnishamk@commvault.com>
2023-05-22 22:12:26 +0530

    update

--- frontend/src/sections/overview/spends.js
+++ frontend/src/sections/overview/spends.js
@@ -1,4 +1,12 @@
-import { Avatar, Card, CardContent, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
 import { useContext } from "react";
 import { ExpenseContext } from "src/contexts/expenses";
 import categories from "./categories";
@@ -6,29 +14,25 @@ import moment from "moment";
 
 export const Spends = () => {
   const { expenses } = useContext(ExpenseContext);
-  console.log(expenses)
+  console.log(expenses);
 
   return (
     <Card>
       <CardContent>
         <List>
-          {
-            expenses.map(expense => (
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>
-                    {
-                      categories[expense.category]?.icon
-                    }
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={expense.payee} secondary={moment(expense.date.$date).format('MMMM Do YYYY, h:mm:ss a')} />
-              </ListItem>
-            ))
-          }
+          {expenses.map((expense) => (
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>{categories[expense.category]?.icon}</Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={expense.payee}
+                secondary={moment(expense.date.$date).format("MMMM Do YYYY, h:mm a")}
+              />
+            </ListItem>
+          ))}
         </List>
       </CardContent>
     </Card>
-  )
+  );
 };
- 
