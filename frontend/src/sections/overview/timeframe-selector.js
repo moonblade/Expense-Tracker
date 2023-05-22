c9bee913abd99e160b131617b79144b0abaa7261
mnishamk <mnishamk@commvault.com>
2023-05-22 19:05:45 +0530

    selector

--- frontend/src/sections/overview/timeframe-selector.js
+++ frontend/src/sections/overview/timeframe-selector.js
@@ -1,38 +1,37 @@
-import {
-  Card,
-  CardContent,
-  MenuItem,
-  Select,
-} from '@mui/material';
-import { useContext, useState } from 'react';
-import { ExpenseContext } from 'src/contexts/expenses';
+import { MenuItem, Select } from "@mui/material";
+import { useContext, useState } from "react";
+import { ExpenseContext } from "src/contexts/expenses";
 import moment from "moment";
 
 export const TimeframeSelector = () => {
-  const { getExpenses } = useContext(ExpenseContext)
+  const { getExpenses } = useContext(ExpenseContext);
 
-  const timeFrames = {"thismonth": {
-    label: "This Month",
-    key: "thismonth",
-    value: {
-      fromTime: moment().startOf('month').format('YYYY-MM-DD'),
-      toTime: moment().format('YYYY-MM-DD')
-    }
-  }, "previousmonth": {
-    label: "Previous Month",
-    key: "previousmonth",
-    value: {
-      fromTime: moment().startOf('month').subtract(1, 'months').format('YYYY-MM-DD'),
-      toTime: moment().endOf('month').subtract(1, 'months').format('YYYY-MM-DD')
-    }
-  }, "thisyear": {
-    label: "This Year",
-    key: "thisyear",
-    value: {
-      fromTime: moment().startOf('year').format('YYYY-MM-DD'),
-      toTime: moment().endOf('month').format('YYYY-MM-DD')
-    }
-  }}
+  const timeFrames = {
+    thismonth: {
+      label: "This Month",
+      key: "thismonth",
+      value: {
+        fromTime: moment().startOf("month").format("YYYY-MM-DD"),
+        toTime: moment().format("YYYY-MM-DD"),
+      },
+    },
+    previousmonth: {
+      label: "Previous Month",
+      key: "previousmonth",
+      value: {
+        fromTime: moment().startOf("month").subtract(1, "months").format("YYYY-MM-DD"),
+        toTime: moment().endOf("month").subtract(1, "months").format("YYYY-MM-DD"),
+      },
+    },
+    thisyear: {
+      label: "This Year",
+      key: "thisyear",
+      value: {
+        fromTime: moment().startOf("year").format("YYYY-MM-DD"),
+        toTime: moment().endOf("month").format("YYYY-MM-DD"),
+      },
+    },
+  };
 
   const [timeFrame, setTimeFrame] = useState(timeFrames["thismonth"].key);
 
@@ -43,19 +42,19 @@ export const TimeframeSelector = () => {
   };
 
   return (
-      <Select
-        labelId="changeTimeFrame"
-        id="changeTimeFrame"
-        value={timeFrame}
-        label="TimeFrame"
-        onChange={handleChange}
-        sx={{ width: '100%' }}
-      >
-        {
-          Object.values(timeFrames).map((tFrame, key) => (
-            <MenuItem value={tFrame.key} key={key}>{tFrame.label}</MenuItem>
-          ))
-        }
-      </Select>
+    <Select
+      labelId="changeTimeFrame"
+      id="changeTimeFrame"
+      value={timeFrame}
+      label="TimeFrame"
+      onChange={handleChange}
+      sx={{ width: "100%" }}
+    >
+      {Object.values(timeFrames).map((tFrame, key) => (
+        <MenuItem value={tFrame.key} key={key}>
+          {tFrame.label}
+        </MenuItem>
+      ))}
+    </Select>
   );
 };
