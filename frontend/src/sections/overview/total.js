0bd1429c8e3097c3f8dc8eaaba54bb7939f89771
moonblade <moonblade168@gmail.com>
2023-05-22 20:08:09 +0530

    remove stuff

--- frontend/src/sections/overview/total.js
+++ frontend/src/sections/overview/total.js
@@ -1,12 +1,10 @@
 import PropTypes from 'prop-types';
-import ArrowDownIcon from '@heroicons/react/24/solid/ArrowDownIcon';
-import ArrowUpIcon from '@heroicons/react/24/solid/ArrowUpIcon';
 import CurrencyRupeeIcon from '@heroicons/react/24/solid/CurrencyRupeeIcon';
-import { Avatar, Card, CardContent, CardHeader, Stack, SvgIcon, Typography } from '@mui/material';
+import { Avatar, Card, CardContent, Stack, SvgIcon, Typography } from '@mui/material';
 import { ExpenseContext } from 'src/contexts/expenses';
 import { useContext } from 'react';
 
-export const OverviewBudget = () => {
+export const Total = () => {
   const { total } = useContext(ExpenseContext);
 
   return (
@@ -38,11 +36,4 @@ export const OverviewBudget = () => {
       </CardContent>
     </Card>
   );
-};
-
-OverviewBudget.prototypes = {
-  difference: PropTypes.number,
-  positive: PropTypes.bool,
-  sx: PropTypes.object,
-  value: PropTypes.string.isRequired
 };
