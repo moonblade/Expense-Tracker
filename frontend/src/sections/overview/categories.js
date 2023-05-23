6222455b71a5ad3aaaf5a7e5725ca8dac2186883
mnishamk <mnishamk@commvault.com>
2023-05-23 10:37:40 +0530

    add colors

--- frontend/src/sections/overview/categories.js
+++ frontend/src/sections/overview/categories.js
@@ -6,37 +6,74 @@ import {
   CakeIcon,
   UserIcon,
 } from "@heroicons/react/24/solid";
+import { Avatar, SvgIcon } from "@mui/material";
 
 const categories = {
   groceries: {
     label: "Groceries",
     category: "groceries",
-    icon: <ShoppingBagIcon />,
+    icon: (
+      <Avatar sx={{backgroundColor: '#C7E9B0'}}>
+        <SvgIcon>
+          <ShoppingBagIcon />
+        </SvgIcon>
+      </Avatar>
+    ),
   },
   unknown: {
     label: "Unknown",
     category: "unknown",
-    icon: <NoSymbolIcon />,
+    icon: (
+      <Avatar sx={{backgroundColor: '#FAAB78'}}>
+        <SvgIcon>
+          <NoSymbolIcon />
+        </SvgIcon>
+      </Avatar>
+    ),
   },
   travel: {
     label: "Travel",
     category: "travel",
-    icon: <TruckIcon />,
+    icon: (
+      <Avatar sx={{backgroundColor: '#A6B1E1'}}>
+        <SvgIcon>
+          <TruckIcon />
+        </SvgIcon>
+      </Avatar>
+    ),
   },
   family: {
     label: "Family",
     category: "family",
-    icon: <UserGroupIcon />,
+    icon: (
+      <Avatar sx={{backgroundColor: '#FFDFD3'}}>
+        <SvgIcon>
+          <UserGroupIcon />
+        </SvgIcon>
+      </Avatar>
+    ),
   },
   food: {
     label: "Food",
     category: "food",
-    icon: <CakeIcon />,
+    icon: (
+      <Avatar sx={{backgroundColor: '#FD8A8A'}}>
+        <SvgIcon>
+          <CakeIcon />
+        </SvgIcon>
+      </Avatar>
+    ),
   },
   friends: {
     label: "Friends",
     category: "friends",
-    icon: <UserIcon />,
+    icon: (
+      <Avatar sx={{backgroundColor: '#D3F6F3'}}>
+        <SvgIcon>
+          <UserIcon />
+        </SvgIcon>
+      </Avatar>
+    ),
   },
 };
 
