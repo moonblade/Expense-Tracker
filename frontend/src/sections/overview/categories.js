a05e228f4f0f0ac2329e16bd25ff29436ecd50a2
moonblade <moonblade168@gmail.com>
2023-05-24 10:27:42 +0530

    update with new icons

--- frontend/src/sections/overview/categories.js
+++ frontend/src/sections/overview/categories.js
@@ -5,15 +5,19 @@ import {
   UserGroupIcon,
   CakeIcon,
   UserIcon,
+  HomeIcon,
 } from "@heroicons/react/24/solid";
 import { Avatar, SvgIcon } from "@mui/material";
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
 
 const categories = {
   groceries: {
     label: "Groceries",
     category: "groceries",
     icon: (
-      <Avatar sx={{backgroundColor: '#C7E9B0'}}>
+      <Avatar sx={{backgroundColor: '#8CD867'}}>
         <SvgIcon>
           <ShoppingBagIcon />
         </SvgIcon>
@@ -57,9 +61,9 @@ const categories = {
     label: "Food",
     category: "food",
     icon: (
-      <Avatar sx={{backgroundColor: '#FD8A8A'}}>
+      <Avatar sx={{backgroundColor: '#EF2D56'}}>
         <SvgIcon>
-          <CakeIcon />
+          <RestaurantIcon />
         </SvgIcon>
       </Avatar>
     ),
@@ -75,6 +79,39 @@ const categories = {
       </Avatar>
     ),
   },
+  health: {
+    label: "Health",
+    category: "health",
+    icon: (
+      <Avatar sx={{backgroundColor: '#C7E9B0'}}>
+        <SvgIcon>
+          <HealthAndSafetyIcon />
+        </SvgIcon>
+      </Avatar>
+    ),
+  }, 
+  body: {
+    label: "Body",
+    category: "body",
+    icon: (
+      <Avatar sx={{backgroundColor: '#C7E9B0'}}>
+        <SvgIcon>
+          <AccessibilityNewIcon />
+        </SvgIcon>
+      </Avatar>
+    ),
+  },
+  home:{
+    label: "Home",
+    category: "home",
+    icon: (
+      <Avatar sx={{backgroundColor: '#C7E9B0'}}>
+        <SvgIcon>
+          <HomeIcon />
+        </SvgIcon>
+      </Avatar>
+    ),
+  } 
 };
 
 export default categories;
--- frontend/src/sections/overview/spends.js
+++ frontend/src/sections/overview/spends.js
@@ -1,5 +1,6 @@
 import {
   Avatar,
+  Box,
   Card,
   CardContent,
   Grid,
@@ -10,6 +11,7 @@ import {
   ListItemText,
   Menu,
   MenuItem,
+  Modal,
   SvgIcon,
   Typography,
 } from "@mui/material";
@@ -26,6 +28,7 @@ export const Spends = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [selectedExpense, setSelectedExpense] = useState(expenses[0] || null);
   const open = Boolean(anchorEl);
+  const [openModal, setOpenModal] = useState(false);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -48,73 +51,122 @@ export const Spends = () => {
     handleClose();
   };
 
+  const setCategory = (category) => {
+    selectedExpense.category = category.category;
+    updateExpense(selectedExpense);
+    setOpenModal(false);
+  };
+
   return (
-    <Card>
-      <CardContent>
-        <List>
-          {expenses.map((expense, key) => (
-            <ListItem key={key}>
-              <ListItemAvatar>
-                <Avatar>{categories[expense.category]?.icon}</Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary={
-                  <Typography
-                    style={{ "text-decoration": expense.enabled == false ? "line-through" : "" }}
+    <>
+      <Card>
+        <CardContent>
+          <List>
+            {expenses.map((expense, key) => (<>{
+              !(expense.deleted) && !(expense.amount == 0) && (
+                <ListItem key={key}>
+                  <ListItemAvatar>{categories[expense.category]?.icon}</ListItemAvatar>
+                  <ListItemText
+                    primary={
+                      <Typography
+                        style={{
+                          "text-decoration": expense.enabled == false ? "line-through" : "",
+                        }}
+                      >
+                        {expense.payee}
+                      </Typography>
+                    }
+                    secondary={
+                      <Fragment>
+                        <Grid container>
+                          <Grid item xs={6}>
+                            <Typography component={"span"}>₹ {expense.amount}</Typography>
+                          </Grid>
+                          <Grid item xs={6}>
+                            <Typography
+                              component={"span"}
+                              varient="caption"
+                              display="block"
+                              fontSize={"0.6rem"}
+                            >
+                              {moment(expense.date.$date).format("MMMM D, h:mm A")}
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Fragment>
+                    }
+                  />
+                  <IconButton
+                    value={key}
+                    id={"basic-button"}
+                    aria-controls={open ? "basic-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                    onClick={handleClick}
                   >
-                    {expense.payee}
-                  </Typography>
-                }
-                secondary={
-                  <Fragment>
-                    <Grid container>
-                      <Grid item xs={6}>
-                        <Typography component={"span"}>₹ {expense.amount}</Typography>
-                      </Grid>
-                      <Grid item xs={6}>
-                        <Typography
-                          component={"span"}
-                          varient="caption"
-                          display="block"
-                          fontSize={"0.6rem"}
-                        >
-                          {moment(expense.date.$date).format("MMMM D, h:mm A")}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Fragment>
-                }
-              />
-              <IconButton
-                value={key}
-                id={"basic-button" + key}
-                aria-controls={open ? "basic-menu" + key : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-                onClick={handleClick}
-              >
-                <SvgIcon>
-                  <EllipsisVerticalIcon />
-                </SvgIcon>
-              </IconButton>
-              <Menu
-                id={"basic-menu" + key}
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby": "basic-button" + key,
-                }}
-              >
-                {selectedExpense?.enabled == false && (
-                  <MenuItem onClick={unignore}>Unignore</MenuItem>
-                )}
-                {selectedExpense?.enabled != false && <MenuItem onClick={ignore}>Ignore</MenuItem>}
-              </Menu>
-            </ListItem>
-          ))}
-        </List>
-      </CardContent>
-    </Card>
+                    <SvgIcon>
+                      <EllipsisVerticalIcon />
+                    </SvgIcon>
+                  </IconButton>
+                </ListItem>
+              )
+            }</>))}
+          </List>
+        </CardContent>
+      </Card>
+      <Menu
+        id={"basic-menu"}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        {selectedExpense?.enabled == false && <MenuItem onClick={unignore}>Unignore</MenuItem>}
+        {selectedExpense?.enabled != false && <MenuItem onClick={ignore}>Ignore</MenuItem>}
+        <MenuItem
+          onClick={() => {
+            setOpenModal(true);
+            handleClose();
+          }}
+        >
+          Categorize
+        </MenuItem>
+      </Menu>
+      <Modal
+        keepMounted
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+      >
+        <Box
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Card>
+            <CardContent>
+              <Grid container>
+                {Object.values(categories).map((category) => (
+                  <Grid item xs={6} sx={{ p: 1 }} onClick={() => setCategory(category)}>
+                    <Box style={{ overflow: "hidden" }}>
+                      {category.icon}
+                      <Typography fontSize={"0.7rem"} overflow={"hidden"}>
+                        {category.label}
+                      </Typography>
+                    </Box>
+                  </Grid>
+                ))}
+              </Grid>
+            </CardContent>
+          </Card>
+        </Box>
+      </Modal>
+    </>
   );
 };
