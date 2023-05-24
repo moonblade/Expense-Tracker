e2f60d9991b55fef887f1041cb43394c22a91901
moonblade <moonblade168@gmail.com>
2023-05-24 18:15:52 +0530

    filter by category

--- frontend/src/sections/overview/donut.js
+++ frontend/src/sections/overview/donut.js
@@ -1,70 +1,72 @@
-import {
-  Card,
-  CardContent,
-} from '@mui/material';
-import { Chart } from 'src/components/chart';
-import { useContext } from 'react';
-import { ExpenseContext } from 'src/contexts/expenses';
-
-const useChartOptions = (labels) => {
-  return {
-    chart: {
-      background: 'transparent'
-    },
-    dataLabels: {
-      minAngleToShowLabel: 10,
-      formatter: function (_, opts) {
-            return opts.w.config.series[opts.seriesIndex]
-      },
-    },
-    labels,
-    legend: {
-      position: 'bottom'
-    },
-    plotOptions: {
-      pie: {
-        // expandOnClick: false
-      }
-    },
-    states: {
-      active: {
-        filter: {
-          type: 'none'
-        }
-      },
-      hover: {
-        filter: {
-          type: 'none'
-        }
-      }
-    },
-    stroke: {
-      width: 0
-    },
-    tooltip: {
-      fillSeriesColor: false
-    }
-  };
-};
+import { Card, CardContent } from "@mui/material";
+import { Chart } from "src/components/chart";
+import { useContext } from "react";
+import { ExpenseContext } from "src/contexts/expenses";
 
 export const Donut = () => {
-  const { categories } = useContext(ExpenseContext)
-  const chartSeries = []
-  const labels = []
-  categories.forEach(category => {
-    chartSeries.push(category.total)
-    labels.push(category.category)
-  })
+  const { categories, filter, updateFilter } = useContext(ExpenseContext);
+  const useChartOptions = (labels) => {
+    return {
+      chart: {
+        background: "transparent",
+        events: {
+          dataPointSelection: function (_, __, config) {
+            const index = config.selectedDataPoints?.[0]?.[0];
+            if (index != undefined) {
+              updateFilter({...filter, category: categories[index].category})
+            } else {
+              updateFilter({...filter, category: undefined})
+            }
+          },
+        },
+      },
+      dataLabels: {
+        minAngleToShowLabel: 10,
+        formatter: function (_, opts) {
+          return opts.w.config.series[opts.seriesIndex];
+        },
+      },
+      labels,
+      legend: {
+        position: "bottom",
+      },
+      plotOptions: {
+        pie: {
+          // expandOnClick: false
+        },
+      },
+      states: {
+        active: {
+          filter: {
+            type: "none",
+          },
+        },
+        hover: {
+          filter: {
+            type: "none",
+          },
+        },
+      },
+      stroke: {
+        width: 0,
+      },
+      tooltip: {
+        fillSeriesColor: false,
+      },
+    };
+  };
+  const chartSeries = [];
+  const labels = [];
+  categories.forEach((category) => {
+    chartSeries.push(category.total);
+    labels.push(category.category);
+  });
   const chartOptions = useChartOptions(labels);
 
   return (
     <Card>
       <CardContent>
-        <Chart
-          options={chartOptions}
-          series={chartSeries}
-          type="donut"
-        />
+        <Chart options={chartOptions} series={chartSeries} type="donut" />
       </CardContent>
     </Card>
   );
