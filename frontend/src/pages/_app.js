c5d7e47092e917e1e684156e6a4f40358eaf1cbb
moonblade <moonblade168@gmail.com>
2023-05-25 20:13:47 +0530

    add auth

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -4,13 +4,13 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
-import { AuthConsumer, AuthProvider } from "src/contexts/auth-context";
 import { useNProgress } from "src/hooks/use-nprogress";
 import { createTheme } from "src/theme";
 import { createEmotionCache } from "src/utils/create-emotion-cache";
 import "simplebar-react/dist/simplebar.min.css";
 import { ExpenseProvider } from "src/contexts/expenses";
 import { SessionProvider } from "next-auth/react";
+import { AuthProvider } from "src/contexts/auth";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -27,26 +27,24 @@ const App = (props) => {
 
   return (
     <SessionProvider session={pageProps.session}>
-      <CacheProvider value={emotionCache}>
-        <Head>
-          <title>Expenses</title>
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-        </Head>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <AuthProvider>
-            <ThemeProvider theme={theme}>
-              <ExpenseProvider>
-                <CssBaseline />
-                <AuthConsumer>
-                  {(auth) =>
-                    auth.isLoading ? <SplashScreen /> : getLayout(<Component {...pageProps} />)
-                  }
-                </AuthConsumer>
-              </ExpenseProvider>
-            </ThemeProvider>
-          </AuthProvider>
-        </LocalizationProvider>
-      </CacheProvider>
+      <AuthProvider>
+        <CacheProvider value={emotionCache}>
+          <Head>
+            <title>Expenses</title>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+          </Head>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <AuthProvider>
+              <ThemeProvider theme={theme}>
+                <ExpenseProvider>
+                  <CssBaseline />
+                  {getLayout(<Component {...pageProps} />)}
+                </ExpenseProvider>
+              </ThemeProvider>
+            </AuthProvider>
+          </LocalizationProvider>
+        </CacheProvider>
+      </AuthProvider>
     </SessionProvider>
   );
 };
